a3431c9670ab3f9c96fa7c559b20c39049fe6a58
Avishek Dutta <avishekdutta531@gmail.com>
2022-08-05 14:03:12 +0600

    publish

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dewin
-Windows Debloating Utility Files
+# Let's Windows Faster!
+
+Debloat Windows.......
+
+## Run
+```ps1
+iwr -useb https://cutt.ly/lZS7biN | iex
+```
--- dewin.ps1
+++ dewin.ps1
@@ -1,1 +1,20 @@
+# Writing Message
+Write-Host "----- Getting Ready -----";
 
+Write-Host "Fixing ngen.exe...";
+$env:PATH = [Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()
+[AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object {
+    $path = $_.Location
+    if ($path) { 
+        $name = Split-Path $path -Leaf
+        Write-Host -ForegroundColor Yellow "`r`nRunning ngen.exe on '$name'"
+        ngen.exe install $path /nologo
+    }
+}
+
+Write-Host "Deleting Temporary Files..."
+Get-ChildItem -Path "C:\Windows\Temp" *.* -Recurse | Remove-Item -Force -Recurse;
+Get-ChildItem -Path "C:\Users\$env:UserName\AppData\Local\Temp" *.* -Recurse | Remove-Item -Force -Recurse;
+
+
+Write-Host "----- Done! -----";
